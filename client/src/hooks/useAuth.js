277d676a3e7b733ddd3fff47d0112189b3ce156e
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-04 15:16:07 +0300

    redux setup and backend merge

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -3,26 +3,29 @@ import { useState, useEffect } from "react";
 import { authService } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import {
+  login as loginReducer,
+  logout as logoutReducer,
+} from "../redux/slices/userSlice";
 
 function useAuth() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [error, setError] = useState(null);
-  const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (authService.isAuthenticated()) {
-      dispatch(login({ ...userData }));
       navigate("/dashboard");
     }
-  }, [isLoggedIn, navigate, userData, dispatch]);
+  }, [isLoggedIn, navigate]);
 
   const login = async (credentials) => {
     try {
       const response = await authService.login(credentials);
       console.log(response.data.payload);
-      setUserData({ ...response.data.payload });
+      const data = response.data.payload;
+      dispatch(loginReducer({ ...data }));
       setIsLoggedIn(true);
     } catch (error) {
       setError("Failed to login. Please check your username and password.");
@@ -32,7 +35,7 @@ function useAuth() {
   const logout = async () => {
     authService.logout();
     setIsLoggedIn(false);
-    // await dispatch(logout());
+    dispatch(logoutReducer());
     navigate("/");
   };
 
