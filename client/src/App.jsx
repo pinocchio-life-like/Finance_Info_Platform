fb2b0d20cd697b2d9e645b812c9dfba7d2c82996
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-05 08:48:17 +0300

    route SoC

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import LoginPage from "./components/Login/LoginPage";
-import DashboardPage from "./components/Dashboard/DashboardPage";
-import ProfilePage from "./components/Profile/ProfilePage";
-import Difference from "./components/DiffViewer/DiffViewer";
-import Editor from "./components/Editor/Editor";
-import Preview from "./components/Preview/Preview";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="dashboard"
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="profile"
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="diffviewer"
-          element={
-            <PrivateRoute>
-              <Difference />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="editor"
-          element={
-            <PrivateRoute>
-              <Editor />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="preview"
-          element={
-            <PrivateRoute>
-              <Preview />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 }
---
+++ client/src/Routes/AppRoutes.jsx
@@ -0,0 +1,67 @@
+// AppRoutes.jsx
+import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "../components/PrivateRoute";
+import LoginPage from "../components/Login/LoginPage";
+import DashboardPage from "../components/Dashboard/DashboardPage";
+import ProfilePage from "../components/Profile/ProfilePage";
+import Difference from "../components/DiffViewer/DiffViewer";
+import Editor from "../components/Editor/Editor";
+import Preview from "../components/Preview/Preview";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <DashboardPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="diffviewer"
+        element={
+          <PrivateRoute>
+            <Difference />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="editor"
+        element={
+          <PrivateRoute>
+            <Editor />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="preview"
+        element={
+          <PrivateRoute>
+            <Preview />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
