d5d691a504985cd62acda90c627679d90b3bd4dd
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-06 14:44:16 +0300

    JWT setup done, routing done, role based complete

--- server/app.js
+++ server/app.js
@@ -1,36 +1,38 @@
-
 require("dotenv").config();
 const express = require("express");
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 //cors
 const cors = require("cors");
-// const User  = require("./config/db.config"); 
-const userAddRoute=require('./routes/usersR')
-const loginRoute = require('./routes/userLoginR');
+// const User  = require("./config/db.config");
+const userAddRoute = require("./routes/usersR");
+const loginRoute = require("./routes/userLoginR");
 
-const userUpdateRoute = require('./routes/userUpdateR');
-const articleRoute = require('./routes/articleR')
-
+const userUpdateRoute = require("./routes/userUpdateR");
+const articleRoute = require("./routes/articleR");
 
 const app = express();
 
 // Middleware setup
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:4000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Routes setup
-app.use('/api',userAddRoute);
-app.use('/api',loginRoute)
+app.use("/api", userAddRoute);
+app.use("/api", loginRoute);
 
-app.use('/api',userUpdateRoute)
-app.use('/api',articleRoute)
+app.use("/api", userUpdateRoute);
+app.use("/api", articleRoute);
 // app.use('/api',articleRoute)
-
 
 // app.get("/", async (req, res) => {
 //   const user = await User.create({
