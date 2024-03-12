2428de85d450b38bd4295156605cd78ced45811e
MEQUANNT ACAHM <mekacham381@gmail.com>
2024-03-12 14:20:51 +0300

    Merge branch 'dev' into mequennt/test

--- client/src/components/Editor/Editor.jsx
+++ client/src/components/Editor/Editor.jsx
@@ -1,47 +1,74 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdEditor } from "md-editor-rt";
 import "md-editor-rt/lib/style.css";
 import "md-editor-rt/lib/preview.css";
 import api from "../../utils/api";
-// import Preview from "../Preview/Preview";
+import useAuth from "../../hooks/useAuth";
 
-const Editor = (props) => {
-  const [text, setText] = useState(
-    "# Hello Editor \n **This is a markdown editor**"
-   
-  );
-  const[article,setArtcile]=useState()
-  console.log(article)
-  useEffect(()=>{
+const Editor = () => {
+  const [text, setText] = useState("");
+  const [articleId, setArticleId] = useState();
+  const [title, setTitle] = useState();
+  const [updateFlag, setUpdateFlag] = useState(false);
+  const [userId, setUserId] = useState();
 
-    const fetchArticle=async()=>{
-      try{
-        const art=await api.get('/api/articles')
-        console.log(art)
-        setArtcile(art.data.data)
-        
-      }
-      catch(error){
-        console.log(error)
-        
-      }
-    };
-  
-     fetchArticle()
-    
-  
-    },[])
-    const generateMarkdomnText=()=>{
-      const markdownContent = article?.map((art) => (
-        `# ${art.articleTitle}\n\n${art.articleContent}\n\n`
-      )).join('');
-  
-      return markdownContent;
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const user = useAuth.login();
+      setUserId(user.userId); //it should be passed from the backend to the payload
 
+      if(articleId){
+        const response = await api.get(`/api/article${articleId}`);
+      console.log(response);
+      const fetchedArticles = response.data.data;
+      if (fetchedArticles && fetchedArticles.length !== 0) {
+        setUpdateFlag(!updateFlag);
+
+        const markdownContent = fetchedArticles
+          .map((art) => `# ${art.articleTitle}\n\n${art.articleContent}\n\n`)
+          .join("");
+
+        setText(markdownContent);
+      } 
     }
-    const generatedMarkdown = generateMarkdomnText()
-    // setText(generatedMarkdown)
-    // setText("#dgfhbfbgg ")
+      else {
+     setText("# now you are in create mode");
+      }
+    }
+
+    fetchArticles();
+  }, [articleId]);
+
+  const handleUserInput = () => {
+    const userInput = window.prompt("Enter the title:");
+
+    if (userInput !== null) {
+      setTitle(userInput);
+      alert(`You entered: ${userInput}`);
+    }
+  };
+
+  const updateArticle = async () => {
+    try {
+      const response = await api.put(`/api/update${articleId}`, {
+        title,
+        text,
+        userId,
+      });
+      alert("Article Update success");
+    } catch (error) {
+      console.error("Error updating article:", error.message);
+    }
+  };
+
+  const createArticle = async () => {
+    try {
+      const response = await api.post("/api/article", { title, text, userId });
+      alert("Article created success");
+    } catch (error) {
+      console.error("Error creating article:", error.message);
+    }
+  };
 
   return (
     <div>
@@ -52,8 +79,13 @@ const Editor = (props) => {
         modelValue={text}
         onChange={setText}
         language="en-US"
-        onSave={() => {
-          // console.log("Save");
+        onSave={async () => {
+          if (updateFlag) {
+            updateArticle();
+          } else {
+            handleUserInput();
+            createArticle();
+          }
         }}
         showCodeRowNumber
       />
