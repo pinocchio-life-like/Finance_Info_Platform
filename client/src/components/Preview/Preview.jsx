6ceae2e34c504ae1609fa9deb7cdbaefb0e8e58d
MEQUANNT ACAHM <mekacham381@gmail.com>
2024-03-14 08:18:48 +0300

    eliays

--- client/src/components/Preview/Preview.jsx
+++ client/src/components/Preview/Preview.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { MdPreview } from "md-editor-rt";
+import { MdCatalog, MdPreview } from "md-editor-rt";
 import "md-editor-rt/lib/style.css";
 import "md-editor-rt/lib/preview.css";
 import { useSelector } from "react-redux";
+const editorId = "my-editor";
 
 const Preview = () => {
   const articleContent = useSelector((state) => state.article.articleContent);
@@ -13,18 +14,36 @@ const Preview = () => {
 
   const [id] = useState("preview-only");
 
+  const [state] = useState({
+    text: "# heading",
+    scrollElement: document.documentElement,
+  });
+
   return (
     <>
-      <MdPreview
-        style={{
-          borderLeft: "1px solid #EEEEEE",
-          borderRight: "1px solid #EEEEEE",
-          boxShadow:
-            "1px 0px 5px rgba(0, 0, 0, 0.1), -1px 0px 5px rgba(0, 0, 0, 0.1)",
-        }}
-        editorId={id}
-        modelValue={text}
-      />
+      <div style={{ display: "flex", width: "100%" }}>
+        <MdPreview
+          style={{
+            borderLeft: "1px solid #EEEEEE",
+            borderRight: "1px solid #EEEEEE",
+            overflowY: "hidden",
+            // boxShadow:
+            //   "1px 0px 5px rgba(0, 0, 0, 0.1), -1px 0px 5px rgba(0, 0, 0, 0.1)",
+          }}
+          editorId={id}
+          modelValue={text}
+        />
+        <MdCatalog
+          scrollElementOffsetTop={false}
+          editorId={id}
+          scrollElement={state.scrollElement}
+          style={{
+            width: "20%",
+            borderLeft: "1px solid #EEEEEE",
+            borderRight: "1px solid #EEEEEE",
+          }}
+        />
+      </div>
     </>
   );
 };
