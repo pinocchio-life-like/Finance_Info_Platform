ca4f40551616c88a6ba0024a1b441887d51c6c33
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-21 11:59:11 +0300

    admin setup done

--- server/routes/usersR.js
+++ server/routes/usersR.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const { createUsers} = require('../controllers/addUsersC');
+const router = require("express").Router();
+const { createUsers, getUsers } = require("../controllers/addUsersC");
 
 // add users route
-router.post('/users', createUsers);
+router.post("/users", createUsers);
+router.get("/users/getall", getUsers);
 //loginroute
-
 
 module.exports = router;
