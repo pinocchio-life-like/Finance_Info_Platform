a08477ec87b0b976d1fa4cc45491457d0bca9dd1
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-22 08:24:18 +0300

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://wihinfo.onrender.com",
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
@@ -20,14 +20,19 @@ api.interceptors.response.use(
       try {
         const response = await api.post("/api/refreshToken");
         const { token } = response.data;
+
+        console.log("Refresh token response:", response); // Log the refresh token response
+
         api.defaults.headers.common["Authorization"] = "Bearer " + token;
         originalRequest.headers["Authorization"] = "Bearer " + token;
         return api(originalRequest);
       } catch (err) {
+        console.log("Error refreshing token:", err); // Log the error if refreshing the token fails
         return Promise.reject(err);
       }
     }
 
+    console.log("Error making request:", error); // Log the error if the request fails
     return Promise.reject(error);
   }
 );
