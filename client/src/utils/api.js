97f362507167400a3d70e8d9c54d1fdbb7b4ecd1
pinocchio-life-like <icbr19fl@gmail.com>
2024-03-28 08:52:38 +0300

    change domain

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://wihinfo.onrender.com",
+  baseURL: "https://finance-info-platform.onrender.com",
   withCredentials: true,
 });
 
@@ -17,24 +17,24 @@ api.interceptors.response.use(
       ) {
         originalRequest._retry = true;
 
-      try {
-        const response = await api.post("/api/refreshToken");
-        const { token } = response.data;
+        try {
+          const response = await api.post("/api/refreshToken");
+          const { token } = response.data;
 
-        console.log("Refresh token response:", response); // Log the refresh token response
+          console.log("Refresh token response:", response); // Log the refresh token response
 
-        api.defaults.headers.common["Authorization"] = "Bearer " + token;
-        originalRequest.headers["Authorization"] = "Bearer " + token;
-        return api(originalRequest);
-      } catch (err) {
-        console.log("Error refreshing token:", err); // Log the error if refreshing the token fails
-        return Promise.reject(err);
+          api.defaults.headers.common["Authorization"] = "Bearer " + token;
+          originalRequest.headers["Authorization"] = "Bearer " + token;
+          return api(originalRequest);
+        } catch (err) {
+          console.log("Error refreshing token:", err); // Log the error if refreshing the token fails
+          return Promise.reject(err);
+        }
       }
-    }
 
-    console.log("Error making request:", error); // Log the error if the request fails
-    return Promise.reject(error);
+      console.log("Error making request:", error); // Log the error if the request fails
+      return Promise.reject(error);
+    }
   }
-}
 );
 export default api;
