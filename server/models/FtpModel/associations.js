3d325f51da079e12bbc08e1886a5c953fd4d37aa
MEQUANNT ACAHM <mekacham381@gmail.com>
2024-05-23 10:54:00 +0300

    update ftp local

--- server/models/FtpModel/associations.js
+++ server/models/FtpModel/associations.js
@@ -1,5 +1,8 @@
 const {Folder}=require('./FolderModel')
 const {File}=require('./FileModel')
+const{User}=require('../userModel')
 
 Folder.hasMany(File,{ foreignKey: 'folder_id' })
 File.belongsTo(Folder, { foreignKey: 'folder_id' });
+Folder.belongsToMany(User, { through: "FolderUser" });
+User.belongsToMany(Folder, { through: "FolderUser" });
