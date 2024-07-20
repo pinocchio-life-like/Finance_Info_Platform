c0a24b6b796ce5edb129be5865e30817316fc91d
pinocchio-life-like <icbr19fl@gmail.com>
2024-07-20 17:53:57 +0300

    push

--- client/src/components/Wiki/WikiFiles/WikiFiles.jsx
+++ client/src/components/Wiki/WikiFiles/WikiFiles.jsx
@@ -1,23 +1,15 @@
 import { Button, List, Popconfirm, Tooltip, message } from "antd";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FiDownload } from "react-icons/fi";
-// import { FaChevronDown } from "react-icons/fa";
 import { useCallback, useEffect, useRef, useState } from "react";
 import api from "../../../utils/api";
 import { useParams } from "react-router";
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
 import { FiCopy } from "react-icons/fi";
 import { UploadOutlined } from "@ant-design/icons";
 import { jwtDecode } from "jwt-decode";
-// import store from "../../../redux/store";
-// import { changeTableOfContentsState } from "../../../redux/slices/contentsSlice";
 
 const WikiFiles = () => {
-  // const [categories, setCategories] = useState([]);
   const param = useParams();
-  // const { drop } = useSelector((state) => state.contents);
-  // const [activeDropdown, setActiveDropdown] = useState(drop);
   const [data, setData] = useState([]);
   const [position] = useState("bottom");
   const [align] = useState("center");
@@ -53,52 +45,16 @@ const WikiFiles = () => {
     fetchData();
   }, [fetchData]);
 
-  // useEffect(() => {
-  //   const getCategories = async () => {
-  //     try {
-  //       const response = await api.post("/api/category/getCategories");
-
-  //       const mainCategories = response.data.filter(
-  //         (category) => category.parent_Id === null
-  //       );
-  //       const subCategories = response.data.filter(
-  //         (category) => category.parent_Id !== null
-  //       );
-
-  //       mainCategories.sort((a, b) => a.order - b.order);
-  //       subCategories.sort(
-  //         (a, b) => a.order_within_parent - b.order_within_parent
-  //       );
-
-  //       mainCategories.forEach((mainCategory) => {
-  //         mainCategory.subCategories = subCategories.filter(
-  //           (subCategory) => subCategory.parent_Id === mainCategory.category_Id
-  //         );
-  //       });
-  //       setCategories(mainCategories);
-  //     } catch (error) {
-  //       console.error("An error occurred while fetching: ", error);
-  //     }
-  //   };
-  //   getCategories();
-  // }, [param.id]);
-
-  // const handleDropdown = (index) => {
-  //   if (activeDropdown === index) {
-  //     setActiveDropdown(null);
-  //   } else {
-  //     setActiveDropdown(index);
-  //   }
-  // };
-
   const handleButtonClick = () => {
     fileInputRef.current.click();
   };
 
   const handleFileChange = async (event) => {
-    const file = event.target.files[0];
+    const files = event.target.files;
     const form = new FormData();
-    form.append("file", file);
+    Array.from(files).forEach((file) => {
+      form.append("files", file);
+    });
     form.append("category_Id", param.id);
     form.append("user", userName);
     setUploading(true);
@@ -130,64 +86,9 @@ const WikiFiles = () => {
     }
   };
 
-  // useEffect(() => {
-  //   store.dispatch(changeTableOfContentsState({ drop: activeDropdown }));
-  // }, [activeDropdown]);
-
   return (
     <div className="w-full border-x border-b px-2">
       <div className="w-full flex justify-start">
-        {/* <div className="border-r w-[16%] pt-4">
-          <div className="w-full flex flex-col"> */}
-        {/* <div className="flex justify-between items-center">
-              <div className="flex justify-start items-center">
-                <h2
-                  style={{ color: "#070F2B" }}
-                  className="text-lg font-bold mb-4">
-                  Shared Article Files
-                </h2>
-              </div>
-            </div> */}
-        {/* {categories.map((category, index) => (
-              <div key={index}>
-                <div className="flex justify-start items-center">
-                  <a
-                    style={{ color: "#070F2B", fontWeight: "bold" }}
-                    key={category.category_Id}
-                    href="#"
-                    className="text-black">
-                    {category.category}
-                  </a>
-                  <button
-                    className="text-black rounded-full w-6 h-6 flex items-center justify-center ml-auto"
-                    onClick={() => handleDropdown(index)}>
-                    <FaChevronDown size={12} />
-                  </button>
-                </div>
-                {activeDropdown === index && (
-                  <div className={`flex flex-col space-y-2 pl-4 $`}>
-                    {category.subCategories.map((subCategory) => {
-                      const linkAddress = `/wiki/files/${subCategory.category_Id}`;
-                      return (
-                        <div
-                          key={subCategory.category_Id}
-                          className="flex justify-between items-center mt-2 group hover:bg-white">
-                          <Link
-                            style={{ color: "#070F2B" }}
-                            key={subCategory.category_Id}
-                            className="text-black"
-                            to={linkAddress}>
-                            {subCategory.category}
-                          </Link>
-                        </div>
-                      );
-                    })}
-                  </div>
-                )}
-              </div>
-            ))} */}
-        {/* </div>
-        </div> */}
         <div className="w-full flex flex-col">
           <div className="w-full flex justify-between border-b">
             <h2
@@ -210,6 +111,7 @@ const WikiFiles = () => {
                 style={{ display: "none" }}
                 onChange={handleFileChange}
                 accept=".pdf,.doc,.docx"
+                multiple
               />
             </div>
           </div>
@@ -222,7 +124,6 @@ const WikiFiles = () => {
             renderItem={(item) => (
               <List.Item key={item.upload_id}>
                 <List.Item.Meta
-                  // avatar={<Avatar src={item.picture.large} />}
                   title={
                     <a
                       style={{ color: "#008DDA" }}
@@ -258,7 +159,6 @@ const WikiFiles = () => {
                       onClick={(event) => {
                         event.preventDefault();
                         if (navigator.clipboard && window.isSecureContext) {
-                          // Use the Clipboard API if available
                           navigator.clipboard
                             .writeText(item.url)
                             .then(() => {
@@ -269,7 +169,6 @@ const WikiFiles = () => {
                               console.error("Could not copy text: ", err)
                             );
                         } else if (document.queryCommandSupported("copy")) {
-                          // Fallback to document.execCommand('copy')
                           const textarea = document.createElement("textarea");
                           textarea.value = item.url;
                           document.body.appendChild(textarea);
